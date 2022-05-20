bebce468e3470cf0659a7a99381d514667f71b14
mouli7625 <moulibhanagar07@gmail.com>
2022-05-20 23:53:42 +0530

    commit -m prog1

---
+++ Prog1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class abc
+{
+    public static void main(String[]args)
+    {
+        Scanner scanner= new Scanner(System.in);
+        List<String> list =new ArrayList<String>();
+        list.add("red");
+        list.add("blue");
+        list.add("black");
+        list.add("green");
+        System.out.print(list);
+    }
+}
