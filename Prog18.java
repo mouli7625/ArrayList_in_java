c6ae210627682ca160d089cbffbf932796b54ece
mouli7625 <moulibhanagar07@gmail.com>
2022-05-22 16:05:18 +0530

    commit -mProg18

---
+++ Prog18.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class abc
+{
+    public static void main(String[]args)
+    {
+        ArrayList<String> l =new ArrayList<String>();
+        l.add("red");
+        l.add("blue");
+        l.add("black");
+        l.add("green");
+        System.out.println(l.isEmpty());
+
+        ArrayList<String> l2 =new ArrayList<String>();
+        System.out.println(l2.isEmpty());
+
+    }
+}
+
+
